0e62b7f77706d2bcff103644f291e6e8621f1e4f
Felipe-33 <fepepiuerj@gmail.com>
2024-10-16 07:42:26 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,18 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # Cria uma expressão regular para busca case insensitive
+    query = {"title": {"$regex": title, "$options": "i"}}
+
+    # Faz a busca no banco de dados
+    news = db.news.find(query)
+
+    # Formata o resultado como uma lista de tuplas (título, url)
+    result = [(item["title"], item["url"]) for item in news]
+
+    return result
 
 
 # Requisito 8
