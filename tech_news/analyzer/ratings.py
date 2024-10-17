b0c2438f8d17da8315ea936d434f60bda1c55e64
Felipe-33 <fepepiuerj@gmail.com>
2024-10-16 21:14:06 -0300

    Corrigindo erro

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -6,7 +6,8 @@ def top_5_categories():
     # Pipeline de agregação para contar as ocorrências de cada categoria
     pipeline = [
         {"$group": {"_id": "$category", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1, "_id": 1}},  # Ordena por contagem decrescente e, em caso de empate, por ordem alfabética
+        # Ordena por contagem decrescente, em caso de empate, ordem alfabética
+        {"$sort": {"count": -1, "_id": 1}},
         {"$limit": 5}  # Limita o resultado às top 5 categorias
     ]
 
