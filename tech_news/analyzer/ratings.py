c24c14b932b317ef8163885453710b8db5a70826
Felipe-33 <fepepiuerj@gmail.com>
2024-10-16 21:02:02 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # Pipeline de agregação para contar as ocorrências de cada categoria
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},  # Ordena por contagem decrescente e, em caso de empate, por ordem alfabética
+        {"$limit": 5}  # Limita o resultado às top 5 categorias
+    ]
+
+    # Executa o pipeline de agregação no MongoDB
+    top_categories = list(db.news.aggregate(pipeline))
+
+    # Extrai as categorias do resultado
+    result = [category["_id"] for category in top_categories]
+
+    return result
